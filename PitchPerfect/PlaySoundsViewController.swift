6fdfcd5f9b2e29a2138b7dda79de836a958f917d
Rosario Martinez <rosario.mgomez@gmail.com>
2015-03-26 14:12:27 -0700

    implemented code to play audio

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
+    
+    var audioPlayer:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
+        } else {
+            println("empty filepath")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +30,23 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func playAudio(rate: Float) {
+        audioPlayer.stop()
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
     }
-    */
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        playAudio(0.5)
+    }
+
+    @IBAction func playFastAudio(sender: UIButton) {
+        playAudio(1.5)
+    }
+    
+    @IBAction func stopPlayAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
 
 }
