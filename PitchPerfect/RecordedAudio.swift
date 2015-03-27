c2cc661c50476f3f2f26be81b5e60dadd4b4038f
Rosario Martinez <rosario.mgomez@gmail.com>
2015-03-26 17:19:44 -0700

    added class to define recorded audio objects

---
+++ PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by ro on 3/26/15.
+//  Copyright (c) 2015 rosariomgomez. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
