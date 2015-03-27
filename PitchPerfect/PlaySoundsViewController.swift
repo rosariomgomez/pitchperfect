6e30d404d2ccc6954f726934a44e3f0bc9c18858
Rosario Martinez <rosario.mgomez@gmail.com>
2015-03-26 18:07:22 -0700

    added code to play chipmunk audio effect

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -14,6 +14,9 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
     
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Code used to play the movie_quote file
@@ -26,6 +29,10 @@ class PlaySoundsViewController: UIViewController {
         //Code to run the recorded sound, received from RecordedSoundVC
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        //to be used by the audioPlayerNode
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,12 +47,37 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.play()
     }
     
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil) //connect to the output (speakers)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
     @IBAction func playSlowAudio(sender: UIButton) {
         playAudio(0.5)
     }
 
     @IBAction func playFastAudio(sender: UIButton) {
         playAudio(1.5)
+    }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
     }
     
     @IBAction func stopPlayAudio(sender: UIButton) {
